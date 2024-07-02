012e566054208ddda443ca7f9b59f7a8713b5fe3
andrewwwmatsko <matskoandrew@gmail.com>
2024-07-02 20:35:06 +0300

    searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,56 @@
-export default function SearchBar() {
-  return <>SearchBar</>;
+import toast, { Toaster } from "react-hot-toast";
+
+import { RiSearchLine } from "react-icons/ri";
+
+import css from "./SearchBar.module.css";
+
+const notify = () =>
+  toast.error("Please enter your query.", {
+    duration: 2000,
+    style: {
+      border: "1px solid #713200",
+      padding: "16px",
+      color: "#713200",
+    },
+    iconTheme: {
+      primary: "#713200",
+      secondary: "#FFFAEE",
+    },
+  });
+
+export default function SearchBar({ onSearch }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = e.target.elements.search.value.trim();
+
+    if (inputValue === "") {
+      notify();
+      return;
+    }
+
+    onSearch(inputValue);
+
+    e.target.reset();
+  };
+
+  return (
+    <header className={css.header}>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <div className={css.formContainer}>
+          <input
+            className={css.search}
+            type="text"
+            name="search"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images..."
+          />
+          <button type="submit" className={css.btn}>
+            <RiSearchLine size={22} color="white" />
+          </button>
+        </div>
+      </form>
+      <Toaster containerStyle={{ top: 50 }} reverseOrder={false} />
+    </header>
+  );
 }
---
+++ src/unsplash-api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export async function fetchPhotos(query) {
+  const API_KEY = "RSuD8NjPnNPwk96VN3D55Gw9p6myIuy3DBNlEDRa_mc";
+
+  axios.defaults.baseURL = "https://api.unsplash.com";
+  axios.defaults.headers.common["Authorization"] = `Client-ID ${API_KEY}`;
+
+  const response = await axios.get("/search/photos", {
+    params: {
+      query,
+      per_page: 15,
+      orientation: "landscape",
+    },
+  });
+
+  return response.data.results;
+}
