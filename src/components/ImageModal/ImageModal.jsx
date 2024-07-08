499f5074e1f946506e1558614743c279b894c928
andrewwwmatsko <matskoandrew@gmail.com>
2024-07-08 23:40:46 +0300

    styles for modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,7 +1,5 @@
 import Modal from "react-modal";
 
-import { Toaster } from "react-hot-toast";
-
 import { RiCloseLargeFill, RiStarLine, RiStarFill } from "react-icons/ri";
 
 import { formatDate } from "../../helpers/formatDate";
@@ -38,10 +36,8 @@ export default function ImageModal({
       left: "50%",
       right: "auto",
       bottom: "auto",
-      marginRight: "-50%",
       transform: "translate(-50%, -50%)",
       backgroundColor: "#F0F0F0",
-      width: "50vw",
     },
   };
 
@@ -68,63 +64,66 @@ export default function ImageModal({
       isOpen={isOpen}
       style={customStyles}
       onRequestClose={onCloseModal}
+      className={css.modal}
       contentLabel="Image Modal window"
       preventScroll={true}
       onAfterClose={handleBodyClassRemove}
     >
-      <div className={css.btnGroup}>
-        <button type="button" className={css.favBtn} onClick={handleAddToFav}>
-          {isAdded ? (
-            <RiStarFill size={28} color="darkblue" />
-          ) : (
-            <RiStarLine size={28} />
-          )}
-        </button>
+      <div className={css.content}>
+        <div className={css.btnGroup}>
+          <button type="button" className={css.favBtn} onClick={handleAddToFav}>
+            {isAdded ? (
+              <RiStarFill size={28} color="darkblue" />
+            ) : (
+              <RiStarLine size={28} />
+            )}
+          </button>
 
-        <button className={css.btn} onClick={onCloseModal} type="button">
-          <RiCloseLargeFill size={28} />
-        </button>
-      </div>
-
-      <img className={css.img} src={urls.regular} alt={alt_description} />
-
-      <div className={css.infoListAndLink}>
-        <ul className={css.infoWrapper}>
-          <li className="imageInfo">
-            <p className={css.imageInfoHeading}>Likes</p>
-            <span className={css.imageInfo}>{likes}</span>
-          </li>
-
-          <li className="imageInfo">
-            <p className={css.imageInfoHeading}>Featured in</p>
-            <ul className={css.featuredIn}>
-              {tags.map((el, i) => (
-                <li key={i}>
-                  <span className={css.imageInfo}> {el.title}</span>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-        <a
-          href={download}
-          rel="noreferrer noopener"
-          target="_blank"
-          download
-          className={css.download}
-          type="button"
-        >
-          Download
-        </a>
-      </div>
-      <p className={css.description}>{description}</p>
-
-      <div className={css.userInfo}>
-        <div>
-          <p className={css.name}>{name}</p>
-          <p>{location}</p>
+          <button className={css.btn} onClick={onCloseModal} type="button">
+            <RiCloseLargeFill size={28} />
+          </button>
         </div>
-        <p className={css.date}>{formatDate(created_at)}</p>
+
+        <img className={css.img} src={urls.regular} alt={alt_description} />
+
+        <div className={css.infoListAndLink}>
+          <ul className={css.infoWrapper}>
+            <li className="imageInfo">
+              <p className={css.imageInfoHeading}>Likes</p>
+              <span className={css.imageInfo}>{likes}</span>
+            </li>
+
+            <li className="imageInfo">
+              <p className={css.imageInfoHeading}>Featured in</p>
+              <ul className={css.featuredIn}>
+                {tags.map((el, i) => (
+                  <li key={i}>
+                    <span className={css.imageInfo}> {el.title}</span>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+          <a
+            href={download}
+            rel="noreferrer noopener"
+            target="_blank"
+            download
+            className={css.download}
+            type="button"
+          >
+            Download
+          </a>
+        </div>
+        <p className={css.description}>{description}</p>
+
+        <div className={css.userInfo}>
+          <div>
+            <p className={css.name}>{name}</p>
+            <p>{location}</p>
+          </div>
+          <p className={css.date}>{formatDate(created_at)}</p>
+        </div>
       </div>
     </Modal>
   );
